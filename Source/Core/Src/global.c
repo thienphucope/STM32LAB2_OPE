25a7a23eb317cdc553ba4133621611ce54e23569
ADMIN\HPVictus <thienphucmain1052004@gmail.com>
2024-09-27 13:46:53 +0700

    wth

--- Source/Core/Src/global.c
+++ Source/Core/Src/global.c
@@ -152,14 +152,14 @@ void update7SEG(int index) {
 	        	if (matrix_buffer[index] & (1 << 7)) HAL_GPIO_WritePin(ENM7_GPIO_Port, ENM7_Pin, 1);
 	        	break;
 	        case 5:
-	        	HAL_GPIO_WritePin(ENM0_GPIO_Port, ENM0_Pin, SET);
-	        	HAL_GPIO_WritePin(ENM1_GPIO_Port, ENM1_Pin, SET);
-	        	HAL_GPIO_WritePin(ENM2_GPIO_Port, ENM2_Pin, SET);
-	        	HAL_GPIO_WritePin(ENM3_GPIO_Port, ENM3_Pin, SET);
-	        	HAL_GPIO_WritePin(ENM4_GPIO_Port, ENM4_Pin, SET);
-	        	HAL_GPIO_WritePin(ENM5_GPIO_Port, ENM5_Pin, RESET);
-	        	HAL_GPIO_WritePin(ENM6_GPIO_Port, ENM6_Pin, SET);
-	        	HAL_GPIO_WritePin(ENM7_GPIO_Port, ENM7_Pin, SET);
+	        	HAL_GPIO_WritePin(ROW0_GPIO_Port, ROW0_Pin, SET);
+	        	HAL_GPIO_WritePin(ROW1_GPIO_Port, ROW1_Pin, SET);
+	        	HAL_GPIO_WritePin(ROW2_GPIO_Port, ROW2_Pin, SET);
+	        	HAL_GPIO_WritePin(ROW3_GPIO_Port, ROW3_Pin, SET);
+	        	HAL_GPIO_WritePin(ROW4_GPIO_Port, ROW4_Pin, SET);
+	        	HAL_GPIO_WritePin(ROW5_GPIO_Port, ROW5_Pin, RESET);
+	        	HAL_GPIO_WritePin(ROW6_GPIO_Port, ROW6_Pin, SET);
+	        	HAL_GPIO_WritePin(ROW7_GPIO_Port, ROW7_Pin, SET);
 	        	if (matrix_buffer[index] & (1 << 0)) HAL_GPIO_WritePin(ENM0_GPIO_Port, ENM0_Pin, 1);
 	        	if (matrix_buffer[index] & (1 << 1)) HAL_GPIO_WritePin(ENM1_GPIO_Port, ENM1_Pin, 1);
 	        	if (matrix_buffer[index] & (1 << 2)) HAL_GPIO_WritePin(ENM2_GPIO_Port, ENM2_Pin, 1);
